133c7bdafc1a6e64c113264d6aabc7cb1fa9fe88
Dan <me@danreynolds.ca>
2016-12-16 12:11:50 -0800

    update api responses to use heredoc for multiline strings

--- app/controllers/champions_controller.rb
+++ app/controllers/champions_controller.rb
@@ -13,29 +13,30 @@ class ChampionsController < ApplicationController
     r: 3,
     ultimate: 3,
     fourth: 3
-  }
+  }.freeze
 
   def description
     roles = @champion[:tags].join(' and ')
-    blurb = remove_html_tags(@champion[:blurb])
     render json: {
-      speech: "#{@champion[:name]} is a #{roles}. #{blurb}"
+      speech: "#{@champion[:name]}, the #{@champion[:title]}, is a #{roles}."
     }
   end
 
   def ability
     ability = champion_params[:ability].to_sym
     if ability == :passive
-      spell = @champion[:passive]
+      spell  @champion[:passive]
     else
       spell = @champion[:spells][ABILITIES[ability]]
     end
 
     render json: {
-      speech: "
-        #{champion_params[:champion]}'s #{ability} ability is called #{spell[:name]}.
-        #{spell[:sanitizedDescription]}
-      "
+      speech: (
+        <<~HEREDOC
+          #{champion_params[:champion]}'s #{ability} ability is called
+          #{spell[:name]}.#{spell[:sanitizedDescription]}
+        HEREDOC
+      )
     }
   end
 
@@ -45,10 +46,13 @@ class ChampionsController < ApplicationController
     rank = champion_params[:rank].split(' ').last.to_i
 
     render json: {
-      speech: "
-        #{champion_params[:champion]}'s #{ability} ability cooldown is
-        #{spell[:cooldown][rank - 1].to_i} seconds at rank #{rank}.
-      "
+      speech: (
+        <<~HEREDOC
+          #{champion_params[:champion]}'s #{ability} ability, #{spell[:name]},
+          has a cooldown of #{spell[:cooldown][rank - 1].to_i} seconds at rank
+          #{rank}.
+        HEREDOC
+      )
     }
   end
 
@@ -60,13 +64,23 @@ class ChampionsController < ApplicationController
 
   def ally_tips
     render json: {
-      speech: @champion[:allytips].sample.to_s
+      speech: (
+        <<~HEREDOC
+          "Here's a tip for playing with #{@champion[:name]}:
+          #{@champion[:allytips].sample.to_s}"
+        HEREDOC
+      )
     }
   end
 
   def enemy_tips
     render json: {
-      speech: @champion[:enemytips].sample.to_s
+      speech: (
+        <<~HEREDOC
+          "Here's a tip for playing against #{@champion[:name]}:
+          #{@champion[:enemytips].sample.to_s}"
+        HEREDOC
+      )
     }
   end
 
