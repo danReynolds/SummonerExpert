b432560fa9049264704b955ec8a9da8f1c432ce9
Dan <me@danreynolds.ca>
2017-01-09 01:16:41 -0500

    added case insensitivity to fuzzy matching

--- lib/matcher.rb
+++ lib/matcher.rb
@@ -15,18 +15,19 @@ module Matcher
     end
 
     def initialize(pattern)
-      @comparator = Levenshtein.new(pattern)
+      @comparator = Levenshtein.new(pattern.downcase)
     end
 
     def find_match(objects, threshold = SIMILARITY_THRESHOLD, key = nil)
       match = objects.inject(Hash.new) do |acc, obj|
-        similarity = key ? @comparator.similar(obj.send(key)) : @comparator.similar(obj)
+        query = key ? obj.send(key) : obj
+        similarity = @comparator.similar(query.downcase)
 
         acc.tap do |acc|
           if (acc[:similarity].nil? || acc[:similarity] < similarity) && similarity >= threshold
             acc.merge!(
-            similarity: similarity,
-            result: obj
+              similarity: similarity,
+              result: obj
             )
           end
         end
