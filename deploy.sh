cc6d64f81e6fb727d62eeb1cea3bf1b0d9d7db97
Dan Reynolds <me@danreynolds.ca>
2017-07-10 14:37:49 -0400

    try building prod image to test on circle

--- deploy.sh
+++ deploy.sh
@@ -9,10 +9,9 @@ docker build -f Dockerfile.prod -t danreynolds/summonerexpert:$DEPLOY_TAG .
 
 # Push Image to Docker Hub
 docker login -u $DOCKER_USER -p $DOCKER_PASS
-docker push danreynolds/summonerexpert:$DEPLOY_TAG
 
 # Create nginx_default network required to run production config
 docker network create nginx_default
 
 # Deploy to Production
-docker-compose -f docker-compose.yml -f docker-compose.production.yml run app rake docker:deploy
+docker-compose -f docker-compose.yml -f docker-compose.production.yml run app rspec
