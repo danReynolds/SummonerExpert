2e8421c46a1a0bcd4ba0cb8baf91bf2365d2bc2c
Dan Reynolds <me@danreynolds.ca>
2017-07-11 00:08:40 -0400

    add ssl nginx config

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,4 @@ class ApplicationController < ActionController::API
   def status
     render json: { success: true }, status: :ok
   end
-
-  def letsencrypt
-    render text: 'D_yF14gneIiGzMult-n_VaGxi8BvPpNsrhtK_eFBZwc.s20dhHv_2FQ191o8TybEHfK4j_N_p5wnIqM5QNARYus'
-  end
 end
