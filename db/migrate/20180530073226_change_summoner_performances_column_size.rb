fefad4ce6b38baf15dd3a6fb9ddc44500f23936b
Dan Reynolds <me@danreynolds.ca>
2018-06-09 23:29:18 -0700

    migrate account id to bigint

--- db/migrate/20180530073226_change_summoner_performances_column_size.rb
+++ db/migrate/20180530073226_change_summoner_performances_column_size.rb
@@ -1,9 +1,5 @@
 class ChangeSummonerPerformancesColumnSize < ActiveRecord::Migration[5.0]
   def up
-    change_column :summoner_performances, :id, :integer, limit: 8
-    change_column :bans, :id, :integer, limit: 8
-    change_column :bans, :summoner_performance_id, :integer, limit: 8
-    change_column :teams, :id, :integer, limit: 8
     change_column :summoners, :account_id, :integer, limit: 8
   end
 end
